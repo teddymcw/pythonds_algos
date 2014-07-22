9cb156d0235ad140f9460a20637fa9e0310b177b
teddymcw <teddymcw@gmail.com>
2014-07-22 16:45:59 -0700

    modified to include full bfs example and graph_ADT reorganized

--- graphs/graph_ADT.py
+++ graphs/graph_ADT.py
@@ -7,26 +7,6 @@
 #in returns True for a statement of the form vertex in graph, if the given vertex is in the graph, False otherwise.
 
 
-class Vertex:
-    def __init__(self,key):
-        self.id = key
-        self.connectedTo = {}
-
-    def addNeighbor(self,nbr,weight=0):
-        self.connectedTo[nbr] = weight
-
-    def __str__(self):
-        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
-
-    def getConnections(self):
-        return self.connectedTo.keys()
-
-    def getId(self):
-        return self.id
-
-    def getWeight(self,nbr):
-        return self.connectedTo[nbr]
-
 class Graph:
     def __init__(self):
         self.vertList = {}
@@ -59,6 +39,27 @@ class Graph:
 
     def __iter__(self):
         return iter(self.vertList.values())
+
+class Vertex:
+    def __init__(self,key):
+        self.id = key
+        self.connectedTo = {}
+
+    def addNeighbor(self,nbr,weight=0):
+        self.connectedTo[nbr] = weight
+
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self,nbr):
+        return self.connectedTo[nbr]
+
 
 #simple test print out
 g = Graph()
